4e128f736300b9949e68a772b6324e685fcab5b1
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-08-30 18:27:39 -0300

    analise das vendas por dia e dia da semana

--- chocolura.py
+++ chocolura.py
@@ -25,6 +25,49 @@ df_chocolura['mes'] = data_formatada
 # Analise da variação de vendas
 df_chocolura['variacao_vendas'] = df_chocolura['vendas'].pct_change()
 
-variacao_vendas = grafico(df_chocolura, 'mes', 'variacao_vendas')
+# variacao_vendas = grafico(df_chocolura, 'mes', 'variacao_vendas')
 
+# plt.show()
+
+
+# Analise de vendas diárias
+df_vendas_diarias = pd.read_csv('dados_vendas/vendas_por_dia.csv') # 61 linhas, 2 colunas
+
+df_vendas_diarias_nulos = df_vendas_diarias.isna().sum() # Não há dados nulos
+
+
+serie_data_vendas = pd.Series(df_vendas_diarias['dia'])
+serie_data_vendas = pd.to_datetime(serie_data_vendas)
+
+data_formatada_vendas = serie_data_vendas.dt.strftime('%b %d')
+
+df_vendas_diarias['dia'] = data_formatada_vendas
+
+vendas_dia_semana = serie_data_vendas.dt.strftime('%a')
+
+df_vendas_diarias['dia_semana'] = vendas_dia_semana
+
+
+# Analise visual de vendas por dia
+# vendas_dia = grafico(df_vendas_diarias, 'dia', 'vendas')
+# plt.show()
+
+
+qtd_vendas_dia_semana = df_vendas_diarias.groupby(['dia_semana'],)['vendas'].sum().sort_values(ascending=False)
+# dia_semana
+# Wed    478
+# Mon    471
+# Tue    465
+# Thu    462
+# Fri    401
+# Sun    151
+# Sat    127
+
+grafico1 = grafico(df_vendas_diarias, 'dia_semana', df_vendas_diarias.groupby(['dia_semana'],)['vendas'].sum())
 plt.show()
+
+
+# print(qtd_vendas_dia_semana)
+
+# print(df_vendas_diarias)
+
